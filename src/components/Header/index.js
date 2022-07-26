8558aa303776cd2bac1e58ea63f8674065fdd782
VanHocAGC <vovanhoc12@gmail.com>
2022-07-26 22:52:07 +0700

    code header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Header(props) {
-  return <div className="bg-amber-300 header-box">Header</div>;
+  const {isOpen, onClickToggle} = props
+  
+  return (
+    <div className={isOpen?"header-box":"header-box hid"}>
+      <div className="header-toggle">
+        <div 
+          onClick={onClickToggle} 
+          className={isOpen?
+          "flex flex-col items-center justify-center space-y-0.5 header-toggle-wrap open":
+          "flex flex-col items-center justify-center space-y-0.5 header-toggle-wrap"
+          }
+        >
+          <div className="header-toggle-item header-toggle-item1"></div>
+          <div className="header-toggle-item header-toggle-item2"></div>
+          <div className="header-toggle-item header-toggle-item3"></div>
+        </div>
+      </div>
+      <div className="uppercase pb-20 pl-6 flex items-center header-list">
+        <ul className="space-y-3">
+          <li style={{transitionDelay: "0.15s"}}><span>Home</span></li>
+          <li style={{transitionDelay: "0.2s"}}><span>Language & framework</span></li>
+          <li style={{transitionDelay: "0.25s"}}><span>Experience</span></li>
+          <li style={{transitionDelay: "0.3s"}}><span>Contact</span></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,16 @@
 import Header from "components/Header";
 import SideBar from "components/SideBar";
-import React from "react";
+import React,{useState} from "react";
 
 function Layout(props) {
+  const [isOpen,setIsOpen] = useState(false)
+  const toggleTrigger = () =>{
+    setIsOpen(!isOpen)
+  }
   return (
     <div className="layout-container">
       <div className="h-full w-100 bg-sky-200 layout-inner">
-        <Header />
+        <Header isOpen={isOpen} onClickToggle={toggleTrigger}/>
         <SideBar />
       </div>
     </div>
